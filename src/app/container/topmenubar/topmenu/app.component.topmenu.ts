ce9bc85e0f61066e6d3d25eb4d251a2796af90c3
Alberto Melendez <amelendez.es@gmail.com>
2017-07-29 21:10:42 +0200

    made topmenu buttons react to click

--- src/app/container/topmenubar/topmenu/app.component.topmenu.ts
+++ src/app/container/topmenubar/topmenu/app.component.topmenu.ts
@@ -1,10 +1,4 @@
 import { Component } from '@angular/core';
-import {
-    topMenuBarTopMenuHomeUrl,
-    topMenuBarTopMenuAboutUrl,
-    topMenuBarTopMenuItemWidth,
-    topMenuBarTopMenuItemHeigh
-} from '../../../common/resources';
 
 @Component({
     selector: 'app-topmenu',
@@ -12,12 +6,16 @@ import {
     styleUrls: ['./app.component.topmenu.css']
 })
 export class TopMenuComponent {
-    /** Home URL */
-    homeUrl = topMenuBarTopMenuHomeUrl;
-
-    /** About Url */
-    aboutUrl = topMenuBarTopMenuAboutUrl;
-
-    width = topMenuBarTopMenuItemWidth;
-    height = topMenuBarTopMenuItemHeigh;
+    handleClick = (option: string): void => {
+        switch (option) {
+            case 'management': console.log('Clicked Management');
+                break;
+            case 'dashboards': console.log('Clicked Dasboards');
+                break;
+            case 'settings': console.log('Clicked Settings');
+                break;
+            case 'about': console.log('Clicked About');
+                break;
+        }
+    }
 }
