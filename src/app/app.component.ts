f5a7294ade3d46b9363807386b774e49d97da5ba
Alberto Melendez <amelendez.es@gmail.com>
2017-07-29 14:41:51 +0200

    created container with topmenubar and logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<app-container></app-container>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { ContainerComponent, TopMenuBarComponent, LogoComponent } from './container/index';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, ContainerComponent, TopMenuBarComponent, LogoComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
